806fa1d927bd70d4d3d25327295d3b6033c8afac
root <root@ip-172-31-41-45.ec2.internal>
2019-10-15 19:14:35 +0000

    commiting for multibranchdemo

--- SemVer.groovy
+++ SemVer.groovy
@@ -9,6 +9,7 @@ def getTagversion (String oldtagVersion)
     timetag = versionParts[2].toString()
     this.month = timetag.substring(4,6).toInteger()
     
+
     
     Calendar now = Calendar.getInstance();
     currmonth = (now.get(Calendar.MONTH) +1)
